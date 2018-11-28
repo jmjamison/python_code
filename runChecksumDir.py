d19798e941f1b98cd2e8558547a4c383e9158a7b
jamie jamison <jamison@library.ucla.edu>
2018-11-28 10:19:19 -0800

    add runChecksumDir to checksum an external drive, change to MD5

--- runChecksumDir.py
+++ runChecksumDir.py
@@ -7,32 +7,35 @@
 # 20181119jmj fix code for python 3.7x - change print statements and add the uft-8 encoding line 25
 # this version, python 3.7 reads external dir
 #
+# add later choice of checksup method
 
 import os, sys, os.path, hashlib, datetime
 
-directoryToCheck = input("Directory to checksum: ")
+directoryToCheck = os.path.normpath(input("Directory to checksum: "))
 
 if os.path.isdir(directoryToCheck): 
-    print(directoryToCheck)
+    print("Yes, it's a directory: " + directoryToCheck + "\n Checksup with MD5")
 
 else:
     print(directoryToCheck + " is not a directory, try again")
 
 pathString = directoryToCheck.split(os.path.sep)
 
-fileDirectory = pathString[len(pathString)-1]
-print(fileDirectory)
+# os.path.normpath mornalize path name, clean up trailing / if there is on
+#fileDirectory = os.path.normpath(directoryToCheck)
+fileDirectory = os.path.normpath(pathString[len(pathString)-1])
+print(" Directory: " + fileDirectory)
 
 
 dirs = os.listdir( directoryToCheck )
 
 
-outfile = open(fileDirectory + "_" + str(datetime.date.today()) + "_" + "_checksumList.txt", "a+")
+outfile = open(fileDirectory + "_" + str(datetime.date.today()) + "_" + "_MD5_checksumList.txt", "a+")
 print(outfile)
 
-# This would print all the files and directories
+# was first using sha1 but changed to md5 since that is what Dataverse uses
 for file in dirs:
-   checksumValue = hashlib.sha1(str(file).encode('utf-8')).hexdigest()   
+   checksumValue = hashlib.md5(str(file).encode('utf-8')).hexdigest()   
    outfile.write("\n" + file + ": " + checksumValue)
    #print("\n" + file)
 
